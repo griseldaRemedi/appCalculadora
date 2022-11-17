3025e415b5f77bd85cab0b66fd08da15aab6657e
Grise <griseldaremedi@gmail.com>
2022-11-16 23:54:44 -0300

    Updates

---
+++ precache-manifest.430b2883e3a58f5f1752e86c42d8c072.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "63cbb343afecf8992c80",
+    "url": "/appCalculadora/static/css/main.c76641d6.chunk.css"
+  },
+  {
+    "revision": "63cbb343afecf8992c80",
+    "url": "/appCalculadora/static/js/main.63cbb343.chunk.js"
+  },
+  {
+    "revision": "69e0dab03ec9f6c99eb0",
+    "url": "/appCalculadora/static/js/1.69e0dab0.chunk.js"
+  },
+  {
+    "revision": "ddeca48710918fe0706e",
+    "url": "/appCalculadora/static/js/2.ddeca487.chunk.js"
+  },
+  {
+    "revision": "433645e842f9e8e16070",
+    "url": "/appCalculadora/static/js/runtime~main.433645e8.js"
+  },
+  {
+    "revision": "a8b4724a9a3733c41a76b349911806f2",
+    "url": "/appCalculadora/static/media/img_san_yo.a8b4724a.png"
+  },
+  {
+    "revision": "43244d32f6c226e8d7cbb8ccc5b7366d",
+    "url": "/appCalculadora/static/media/logo_san_yo.43244d32.png"
+  },
+  {
+    "revision": "2156721d90f906dc9448f3a30c30737f",
+    "url": "/appCalculadora/static/media/formulas.2156721d.jpg"
+  },
+  {
+    "revision": "c3708e29f2c6eb94451bf394d18cf31d",
+    "url": "/appCalculadora/index.html"
+  }
+];
